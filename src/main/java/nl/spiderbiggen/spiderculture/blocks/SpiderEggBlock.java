cd3137802ff7a1189e07b662027084d65972373d
Spiderbiggen <spiderbiggen@gmail.com>
2014-08-15 01:40:30 +0200

    set a base for the mod

---
+++ src/main/java/nl/spiderbiggen/spiderculture/blocks/SpiderEggBlock.java
@@ -0,0 +1,13 @@
+package nl.spiderbiggen.spiderculture.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+/**
+ * Created by Spiderbiggen on 15-8-2014.
+ */
+public class SpiderEggBlock extends Block {
+    public SpiderEggBlock(Material mat) {
+        super(mat);
+    }
+}
