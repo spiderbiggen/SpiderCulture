bf0006139370ac467fc513352f2de3590976a531
Spiderbiggen <spiderbiggen@gmail.com>
2014-08-15 00:36:01 +0200

    commit one

---
+++ src/main/java/nl/spiderbiggen/spiderculture/Reference.java
@@ -0,0 +1,9 @@
+package nl.spiderbiggen.spiderculture;
+
+/**
+ * Created by Spiderbiggen on 15-8-2014.
+ */
+public class Reference {
+    public static final String MODID = "spiderculture";
+    public static final String VERSION = "Dev 0.0.1";
+}
---
+++ src/main/java/nl/spiderbiggen/spiderculture/SpiderCulture.java
@@ -0,0 +1,11 @@
+package nl.spiderbiggen.spiderculture;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * Created by Spiderbiggen on 15-8-2014.
+ */
+
+@Mod(modid = Reference.MODID, version = Reference.VERSION)
+public class SpiderCulture {
+}
