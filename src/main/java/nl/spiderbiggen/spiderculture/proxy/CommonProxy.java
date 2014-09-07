14e8be45d658d5cff4dd70ea96c329a64894c947
Spiderbiggen <spiderbiggen@gmail.com>
2014-09-07 02:32:25 +0200

    1st attempt at a mob

--- src/main/java/nl/spiderbiggen/spiderculture/proxy/CommonProxy.java
+++ src/main/java/nl/spiderbiggen/spiderculture/proxy/CommonProxy.java
@@ -1,6 +1,10 @@
 package nl.spiderbiggen.spiderculture.proxy;
 
+import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
+import nl.spiderbiggen.spiderculture.SpiderCulture;
+import nl.spiderbiggen.spiderculture.entity.EntityCulturedSpider;
+import nl.spiderbiggen.spiderculture.reference.EntityIds;
 import nl.spiderbiggen.spiderculture.tileentity.TileEntitySpiderEgg;
 
 /**
@@ -9,5 +13,9 @@ import nl.spiderbiggen.spiderculture.tileentity.TileEntitySpiderEgg;
 public abstract class CommonProxy implements IProxy {
     public void registerTileEntities() {
         GameRegistry.registerTileEntity(TileEntitySpiderEgg.class, "spiderEggBlock");
+    }
+
+    public void registerEntities() {
+        EntityRegistry.registerModEntity(EntityCulturedSpider.class, "culturedSpider", EntityIds.culturedSpider, SpiderCulture.instance, 80, 1, true);
     }
 }
