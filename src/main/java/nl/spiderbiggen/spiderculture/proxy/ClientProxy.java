45584135555b976a3de8401c940ff0abfca09441
Spiderbiggen <spiderbiggen@gmail.com>
2014-09-07 02:32:19 +0200

    1st attempt at a mob

--- src/main/java/nl/spiderbiggen/spiderculture/proxy/ClientProxy.java
+++ src/main/java/nl/spiderbiggen/spiderculture/proxy/ClientProxy.java
@@ -2,11 +2,14 @@ package nl.spiderbiggen.spiderculture.proxy;
 
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import net.minecraft.client.model.ModelSpider;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.MinecraftForgeClient;
 import nl.spiderbiggen.spiderculture.SpiderCulture;
+import nl.spiderbiggen.spiderculture.client.renderer.entity.RenderCulturedSpider;
 import nl.spiderbiggen.spiderculture.client.renderer.item.ItemRendererSpiderEgg;
 import nl.spiderbiggen.spiderculture.client.renderer.tileentity.TileEntityRendererSpiderEgg;
+import nl.spiderbiggen.spiderculture.entity.EntityCulturedSpider;
 import nl.spiderbiggen.spiderculture.reference.RenderIds;
 import nl.spiderbiggen.spiderculture.tileentity.TileEntitySpiderEgg;
 
@@ -18,11 +21,11 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void initRenderingAndTextures() {
         RenderIds.spiderEggBlock = RenderingRegistry.getNextAvailableRenderId();
-
         MinecraftForgeClient.registerItemRenderer(SpiderCulture.spiderEggItem, new ItemRendererSpiderEgg());
         MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(SpiderCulture.spiderEggBlock), new ItemRendererSpiderEgg());
-
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntitySpiderEgg.class, new TileEntityRendererSpiderEgg());
+        RenderingRegistry.registerEntityRenderingHandler(EntityCulturedSpider.class, new RenderCulturedSpider(new ModelSpider(), 0.5f));
     }
+
 }
 
